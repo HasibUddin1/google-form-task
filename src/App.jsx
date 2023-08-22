d7f58ef8e4c80fc6c7df6eee6dada3525cac31d4
IconicLuck <uhasib959@gmail.com>
2023-08-22 22:00:30 +0600

    added form heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import FormHeading from './components/FormHeading/FormHeading'
 
 function App() {
-  
+
 
   return (
-    <div className='w-1/6 mx-auto'>
-      
+    <div className='bg-[#f0ebf8]'>
+      <div className='w-1/3 mx-auto pt-5 pb-5'>
+        <FormHeading></FormHeading>
+      </div>
     </div>
   )
 }
